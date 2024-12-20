bfc0f07638587e39cb2a016d7e8830a0eadb728d
Daniel Ramos <danielramosacosta1@gmail.com>
2024-12-20 16:47:29 +0100

    refactor: move to array of lights

--- src/ChristmasLights.ts
+++ src/ChristmasLights.ts
@@ -4,11 +4,15 @@ export class ChristmasLights {
   private isOn = false
   private isOn2 = false;
 
+  private row = [false, false]
+
   turnOn(start: Coordinates, end: Coordinates) {
     this.isOn = true
 
+    this.row[0] = true
+
     if (end.y === 1) {
-      this.isOn2 = true
+      this.row[1] = true
     }
   }
 
@@ -17,16 +21,11 @@ export class ChristmasLights {
   }
 
   amountLit() {
-    let lightsAmount = 0;
-
-    if (this.isOn) {
-      lightsAmount += 1
-    }
-
-    if (this.isOn2) {
-      lightsAmount += 1
-    }
-
-    return lightsAmount
+    return this.row.reduce((acc, current) => {
+      if (current) {
+        return acc + 1
+      }
+      return acc
+    }, 0)
   }
 }
