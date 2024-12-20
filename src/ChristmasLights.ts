cfc0b3047422a8d4281ce332a48d93ea81ffea4f
Daniel Ramos <danielramosacosta1@gmail.com>
2024-12-20 16:48:16 +0100

    refactor: remove unused properties

--- src/ChristmasLights.ts
+++ src/ChristmasLights.ts
@@ -1,23 +1,18 @@
 import { Coordinates } from "./Coordinates.js"
 
 export class ChristmasLights {
-  private isOn = false
-  private isOn2 = false;
-
   private row = [false, false]
 
-  turnOn(start: Coordinates, end: Coordinates) {
-    this.isOn = true
+  static create() {
+    return new ChristmasLights()
+  }
 
+  turnOn(start: Coordinates, end: Coordinates) {
     this.row[0] = true
 
     if (end.y === 1) {
       this.row[1] = true
     }
-  }
-
-  static create() {
-    return new ChristmasLights()
   }
 
   amountLit() {
